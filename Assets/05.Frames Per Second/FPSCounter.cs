cc109d4e6ecaa7ec3a9b7061bac0d2a8609da1b3
红色篝火 <48897680+kav1982@users.noreply.github.com>
2019-04-23 20:24:35 +0800

    效率测试练习启动

---
+++ Assets/05.Frames Per Second/FPSCounter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSCounter : MonoBehaviour
+{
+    public int FPS {get; private set;}
+
+    void Update()
+    {
+        FPS = (int) (1f / Time.unscaledDeltaTime);
+    }
+}
