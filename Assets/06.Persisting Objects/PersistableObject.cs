23cf4945e8812e6c2c7265d8a42e256ac5107668
红色篝火 <48897680+kav1982@users.noreply.github.com>
2019-04-24 17:53:08 +0800

    创建保存加载练习启动

---
+++ Assets/06.Persisting Objects/PersistableObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+[DisallowMultipleComponent] public class PersistableObject : MonoBehaviour
+{
+    public void Save (GameDataWriter writer)
+    {
+        writer.Write(transform.localPosition);
+        writer.Write(transform.localRotation);
+        writer.Write(transform.localScale);
+    }
+
+    public void Load (GameDataReader reader)
+    {
+        transform.localPosition = reader.ReadVector3();
+        transform.localRotation = reader.ReadQuaternion();
+        transform.localScale = reader.ReadVector3();
+    }
+}
